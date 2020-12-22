fcd5132bdeb82ee940cae1c15a606cda2eb13d41
Sebastian Döll <sebastian@katallaxie.me>
2020-12-22 10:14:43 +0100

    wip: fixing go path

--- gen.go
+++ gen.go
@@ -1,7 +1,7 @@
 package api
 
 //go:generate mkdir -p proto
-//go:generate protoc --go_out=plugins=grpc:. --go_opt=module=github.com/cgentron -I. cgentron/api/annotations.proto
-//go:generate protoc --go_out=plugins=grpc:. --go_opt=module=github.com/cgentron  -I. cgentron/api/fields.proto
-//go:generate protoc --go_out=plugins=grpc:. --go_opt=module=github.com/cgentron  -I. cgentron/api/methods.proto
-//go:generate protoc --go_out=plugins=grpc:. --go_opt=module=github.com/cgentron  -I. cgentron/api/resolver.proto
+//go:generate protoc --go_out=plugins=grpc:. --go_opt=module=github.com/cgentron/api -I. cgentron/api/annotations.proto
+//go:generate protoc --go_out=plugins=grpc:. --go_opt=module=github.com/cgentron/api -I. cgentron/api/fields.proto
+//go:generate protoc --go_out=plugins=grpc:. --go_opt=module=github.com/cgentron/api -I. cgentron/api/methods.proto
+//go:generate protoc --go_out=plugins=grpc:. --go_opt=module=github.com/cgentron/api -I. cgentron/api/resolver.proto
